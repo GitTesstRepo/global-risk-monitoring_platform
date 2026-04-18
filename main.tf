7bd92022b0629bdf7995bf91866053e226153af4
GitTesstRepo <52160246+GitTesstRepo@users.noreply.github.com>
2026-04-18 23:32:33 +0000

    Added Terraform part to create bucket

--- main.tf
+++ main.tf
@@ -12,3 +12,18 @@ provider "google" {
   project     = "terra-77564"
   region      = "us-central1"
 }
+
+resource "google_storage_bucket" "data-lake" {
+  name          = "terra-77564-data-lake"
+  location      = "US"
+  force_destroy = true
+  
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
