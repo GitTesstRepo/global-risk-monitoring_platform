006f4fe8129aaf742d2ea22a3d2dacbeaf8f7711
NoName <ffuiol2lfwqv@mail.ru>
2026-04-19 23:58:28 +0300

    Fixed name of service file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "credentials" {
   description = "My Credentials"
-  default     = "./gcs.json"
+  default     = "./gcp.json"
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
